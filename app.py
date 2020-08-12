5b38b18648a0492650a8eb60a217dec884c1a515
dsaovuilde <dwsoule@hotmail.com>
2020-08-11 18:16:01 -0600

    re-optimized model

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import dash_html_components as html
 import dash_table
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.linear_model import LogisticRegression
@@ -22,35 +21,28 @@ heart = pd.read_csv(url)
 # train_test split to split the dataframe into test data and train data for prediction models
 y = pd.DataFrame(heart['target'])
 X = heart.drop('target', axis=1)
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-X_train2, X_test2, y_train2, y_test2 = train_test_split(heart.drop('target', axis=1), y, test_size=0.2, random_state=1)
-X_test2['target'] = y_test2
-rf = RandomForestClassifier(n_estimators=644, min_samples_split=10, min_samples_leaf=1, max_features='sqrt',
-                            max_depth=40, bootstrap=False, random_state=4)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123456)
+
+rf = RandomForestClassifier(n_estimators=733, min_samples_split=5, min_samples_leaf=4, max_features='auto',
+                            max_depth=80, bootstrap=True, random_state=7896543)
 rf.fit(X_train, y_train)
 prediction = rf.predict(X_test)
 accuracy = accuracy_score(y_test, prediction)
 
-y_test2['predicted'] = prediction
-X_test2['predicted'] = prediction
 
+cm = confusion_matrix(y_test, prediction)
+print(cm)
 # calculate sensitivity and specificity
-tp_filter = (y_test2['predicted'] == 1) & (y_test2['target'] == 1)
-true_positives = len(y_test[tp_filter])
-tn_filter = (y_test2['predicted'] == 0) & (y_test2['target'] == 0)
-true_negatives = len(y_test[tn_filter])
-fp_filter = (y_test2['predicted'] == 1) & (y_test2['target'] == 0)
-false_positives = len(y_test[fp_filter])
-fn_filter = (y_test2['predicted'] == 0) & (y_test2['target'] == 1)
-false_negatives = len(y_test[fn_filter])
+
+true_positives = cm[0][0]
+true_negatives = cm[1][1]
+false_positives = cm[0][1]
+false_negatives = cm[1][0]
 sensitivity = true_positives / (true_positives + false_negatives)
 specificity = true_negatives / (true_negatives + false_positives)
 
 
 def plot_confusion_matrix():
-    cm = confusion_matrix(y_test, prediction)
     labels = ['False', 'True']
     data = go.Heatmap(z=cm, y=labels, x=labels, colorscale='tealgrn')
     annotations = []
@@ -168,50 +160,55 @@ app.layout = html.Div([
             html.P('num - narrowing'),
             html.P('target - presence of heart disease')
         ], style={'display': 'inline-block'}),
+html.Div([
+            dcc.Graph(figure=fig1),
+        ], style={'display': 'inline-block'}),
     ]),
     html.Div([
-        dcc.Graph(figure=fig1),
-    ]),
-    html.Div([
-        dcc.RadioItems(
-            id='feature',
-            options=[{'label': i, 'value': i} for i in ['Age', 'Blood Pressure', 'Max Heart Rate']],
-            value='Age',
-            labelStyle={'display': 'inline-block'}
-        ),
-        daq.Slider(
-            id='slider',
-            min=29,
-            max=220,
-            handleLabel={'showCurrentValue': True, 'label': "VALUE"},
-            value=heart['age'].min()
-        ),
-        html.Div(id='slider_output'),
-    ]),
-    html.Div([
-        html.H1('Predictive Model', style={'textAlign': 'center'}),
-        html.Div([dash_table.DataTable(
-            id='table',
-            columns=[{'name': i, 'id': i} for i in X_test2.columns],
-            data=pd.DataFrame(X_test2).to_dict('records'),
-            editable=True,
-            filter_action='native',
-            sort_action='native',
-            sort_mode='multi',
-            row_selectable='multi',
-            selected_columns=[],
-            selected_rows=[],
-            style_table={
-                'height': '300px',
-                'overflowY': 'scroll'
-            }
-        ),
-            html.P(
-                'Table can be sorted and filtered. To filter search for strings rather than numbers. (\'6.2\' instead of 6.2)'), ]),
-        html.Div(dcc.Graph(figure=plot_confusion_matrix())),
-        html.P(u'Overall Accuracy : {}'.format(accuracy)),
-        html.P(u'Sensitivity: {}'.format(sensitivity)),
-        html.P(u'Specificity: {}'.format(specificity))
+        html.H1('Odds of Heart Disease'),
+        html.P('Select a variable and increase or decrease the slider to see the effect of incremental changes on odd of having heart disease.'),
+        html.P('NOTE: Blood pressure has a negative correlation in this dataset likely due to the number of patients with high BP who are otherwise healthy thus no disease.'),
+        html.Div([
+            dcc.RadioItems(
+                id='feature',
+                options=[{'label': i, 'value': i} for i in ['Age', 'Blood Pressure', 'Max Heart Rate']],
+                value='Age',
+                labelStyle={'display': 'inline-block'}
+            ),
+            daq.Slider(
+                id='slider',
+                min=29,
+                max=220,
+                handleLabel={'showCurrentValue': True, 'label': "VALUE"},
+                value=heart['age'].min()
+            ),
+            html.Div(id='slider_output'),
+        ]),
+        html.Div([
+            html.H1('Predictive Model', style={'textAlign': 'center'}),
+            html.Div([dash_table.DataTable(
+                id='table',
+                columns=[{'name': i, 'id': i} for i in X_test.columns],
+                data=pd.DataFrame(X_test).to_dict('records'),
+                editable=True,
+                filter_action='native',
+                sort_action='native',
+                sort_mode='multi',
+                row_selectable='multi',
+                selected_columns=[],
+                selected_rows=[],
+                style_table={
+                    'height': '300px',
+                    'overflowY': 'scroll'
+                }
+            ),
+                html.P(
+                    'Table can be sorted and filtered. To filter search for strings rather than numbers. (\'6.2\' instead of 6.2)'), ]),
+            html.Div(dcc.Graph(figure=plot_confusion_matrix())),
+            html.P(u'Overall Accuracy : {}'.format(accuracy)),
+            html.P(u'Sensitivity: {}'.format(sensitivity)),
+            html.P(u'Specificity: {}'.format(specificity))
+        ])
     ])
 ])
 
@@ -222,8 +219,8 @@ app.layout = html.Div([
      dash.dependencies.Input('feature', 'value')])
 def update_output(slider, radio):
     if radio == 'Age':
-        coef = -.05395622
-        dif = (int(slider) - 29) / float(heart.age.std())
+        coef = -.01266125
+        dif = (int(slider) - 29)
         odds_ratio = math.exp(dif * coef)
         if (odds_ratio > 1):
             percent = (odds_ratio * 100) - 100
@@ -232,8 +229,8 @@ def update_output(slider, radio):
             percent = 100 - (odds_ratio * 100)
             return 'Odds of heart disease has decreased by {}%'.format(percent)
     if radio == 'Blood Pressure':
-        coef = -.3189122
-        dif = (int(slider) - heart.trestbps.min()) / float(heart.trestbps.std())
+        coef = -.01468438
+        dif = (int(slider) - heart.trestbps.min())
         odds_ratio = math.exp(dif * coef)
         if (odds_ratio > 1):
             percent = (odds_ratio * 100) - 100
@@ -242,8 +239,8 @@ def update_output(slider, radio):
             percent = 100 - (odds_ratio * 100)
             return 'Odds of heart disease has decreased by {}%'.format(percent)
     if radio == 'Max Heart Rate':
-        coef = .5045519
-        dif = (int(slider) - heart.thalach.min()) / float(heart.thalach.std())
+        coef = .02946909
+        dif = (int(slider) - heart.thalach.min())
         odds_ratio = math.exp(dif * coef)
         if (odds_ratio > 1):
             percent = (odds_ratio * 100) - 100
